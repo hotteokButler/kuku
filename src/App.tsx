9a32bf9fcf59e39e250167a0fef693fef11d395c
hotteokButler <ygs12567@gmail.com>
2023-12-08 17:09:42 +0900

    SET : KUKU Project by Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/img/react.svg'
+import viteLogo from '/vite.svg'
+import { MainWrapper } from './components/style/sty_common'
+
+
+function App() {
+
+  return (
+    <MainWrapper>
+      
+    </MainWrapper>
+  )
+}
+
+export default App
---
+++ src/components/style/sty_common.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import reset from "styled-reset";
+
+export const MainWrapper = styled.div`
+  ${reset}
+`
